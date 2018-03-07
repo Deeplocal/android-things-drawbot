73799af01c2f63f0707341ea440b4aa549dcf755
robert-rudolph <rrudolph@deeplocal.com>
2018-03-07 10:44:05 -0500

    Nick Hanna training day changes

--- update-calibration.bat
+++ update-calibration.bat
@@ -1,5 +1,5 @@
-@echo on
-set adb_command=C:\Users\Deeplocal\Documents\platform-tools\adb
+@echo off
+set adb_command=adb
 set file_path=/storage/emulated/0/Android/data/com.deeplocal.drawbot/files/
 IF EXIST "%1" (
 	echo Moving %1 to %file_path%%1
